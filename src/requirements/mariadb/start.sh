686ce34d0fc52ac1997e20b07344b4e365da9d8f
Sterling Clam <sclam@un-k1.kzn.21-school.ru>
2022-06-17 20:09:14 +0300

    still_maria_db

---
+++ src/requirements/mariadb/start.sh
@@ -0,0 +1,26 @@
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
+mysql_install_db
+service mysql start
+echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql
+echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+echo "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql
+echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql
+echo "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '${MYSQL_USER}'@'%';" | mysql
+echo "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '${MYSQL_USER}'@'localhost';" | mysql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_RANDOM_ROOT_PASSWORD}';" | mysql
+echo "FLUSH PRIVILEGES;" | mysql
+fi
+/usr/bin/mysqld_safe
+# cat << EOF > $tfile
+# USE mysql;
+# FLUSH PRIVILEGES;
+# GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_RANDOM_ROOT_PASSWORD" WITH GRANT OPTION;
+# GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+# EOF
+
+# echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+# echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+# mysqld --user=root --bootstrap --verbose=0 < $tfile
+# rm -f $tfile
+# mysqld --user=root --console
