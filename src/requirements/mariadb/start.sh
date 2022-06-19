6f056398ed30087baa97c50ddf89bee3592efb3b
Sterling Clam <sclam@un-l2.kzn.21-school.ru>
2022-06-19 15:01:42 +0300

    maria_db_cmds_problem

--- src/requirements/mariadb/start.sh
+++ src/requirements/mariadb/start.sh
@@ -1,27 +1,14 @@
 if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
-mysql_install_db --user=root --datadir=/var/lib/mysql
-service mysql start
-echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql
-echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
-echo "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql
-echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql
-echo "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '${MYSQL_USER}'@'%';" | mysql
-echo "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '${MYSQL_USER}'@'localhost';" | mysql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_RANDOM_ROOT_PASSWORD}';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
-service mysql stop
+	mysql_install_db --user=root --datadir=/var/lib/mysql
+	service mysql start
+	echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql
+	echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+	# echo "CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql
+	echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql
+	echo "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '${MYSQL_USER}'@'%';" | mysql
+	# echo "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '${MYSQL_USER}'@'localhost';" | mysql
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_RANDOM_ROOT_PASSWORD}';" | mysql
+	echo "FLUSH PRIVILEGES;" | mysql
+	service mysql stop
 fi
-/usr/bin/mysqld_safe
-# cat << EOF > $tfile
-# USE mysql;
-# FLUSH PRIVILEGES;
-# GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_RANDOM_ROOT_PASSWORD" WITH GRANT OPTION;
-# GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '';
-# EOF
-
-# echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
-# echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
-# mysqld --user=root --bootstrap --verbose=0 < $tfile
-# rm -f $tfile
-# mysqld --user=root --console
+exec mysqld_safe
--- src/requirements/wordpress/start.sh
+++ src/requirements/wordpress/start.sh
@@ -1,17 +1,6 @@
 if ! [ -f "/var/www/wordpress/wp-config.php" ]; then
-# cd /var/www/wordpress && wp core download --allow-root --force
-wp core download --allow-root --force --path=/var/www/wordpress
-# mv /wp-config.php ./wp-config.php
-wp core config --allow-root --dbhost=$WORDPRESS_DB_HOST:3306 --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbname=$WORDPRESS_DB_NAME --path=/var/www/wordpress
-wp core install --allow-root --url=sclam.42.fr --title=Inception --admin_user=sclam --admin_password=sclam --admin_email=admin@admin.com --skip-email --path=/var/www/wordpress
+	wp core download --allow-root --force --path=/var/www/wordpress
+	wp core config --allow-root --dbhost=$WORDPRESS_DB_HOST:3306 --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbname=$WORDPRESS_DB_NAME --path=/var/www/wordpress
+	wp core install --allow-root --url=sclam.42.fr --title=Inception --admin_user=sclam --admin_password=sclam_pass --admin_email=admin@admin.com --skip-email --path=/var/www/wordpress
 fi
-# wp user create --allow-root 
 php-fpm7.3 -F
-# cd /var/www/wordpress
-# if ! wp core is-installed --allow-root; then
-# 	wp core download --allow-root
-# 	wp config --allow-root --dbname=$WORDPRESS_DB_HOST:3306 --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --force
-# 	wp core install --allow-root --url=sclam.42.fr --title=Inception --admin_user=sclam --admin_password=sclam --admin_email="admin@admin.com" --skip-email
-# fi
-# # wp user create --allow-root 
-# php-fpm7.3 -F
