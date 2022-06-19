0ee362a8c16832085d9510b1f267b2aeb3288678
sclam <zvmkm@mail.ru>
2022-06-20 00:41:05 +0300

    still_mariadb

--- src/requirements/wordpress/start.sh
+++ src/requirements/wordpress/start.sh
@@ -4,6 +4,6 @@ if ! [ -f "/var/www/wordpress/wp-config.php" ]; then
 		sleep 1;
 		wp core config --allow-root --dbhost=$WORDPRESS_DB_HOST:3306 --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbname=$WORDPRESS_DB_NAME --path=/var/www/wordpress
 	done
-	wp core install --allow-root --url=sclam.42.fr --title=Inception --admin_user=sclam --admin_password=sclam_pass --admin_email=admin@admin.com --skip-email --path=/var/www/wordpress
+	wp core install --allow-root --url=sclam.42.fr --title=Inception --admin_user=sclam --admin_password=sclam_pass --admin_email=admin@admin.com --path=/var/www/wordpress
 fi
 php-fpm7.3 -F
